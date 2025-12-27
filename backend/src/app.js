a72f705b211ad7cb0e85e6f076c37fa29e403da7
maaz <maazshaikh2079@gmail.com>
2025-12-27 22:47:39 +0530

    fixed BE according to vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -82,18 +82,46 @@ app.use((error, req, res, next) => {
   });
 });
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g9wuk9q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() =>
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("log> Connected to database!");
-      console.log(`log> app listening on PORT:${process.env.PORT || 5000}`);
-    })
-  )
-  .catch((error) => {
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g9wuk9q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+//   )
+//   .then(() =>
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log("log> Connected to database!");
+//       console.log(`log> app listening on PORT:${process.env.PORT || 5000}`);
+//     })
+//   )
+//   .catch((error) => {
+//     console.log("log> MongoDB connection FAILED!!!");
+//     console.log("log> Error:-");
+//     console.error(error);
+//   });
+
+let isConnected = false;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g9wuk9q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    isConnected = true;
+    console.log("log> Connected to database!");
+    console.log(`log> app listening on PORT:${process.env.PORT || 5000}`);
+  } catch (error) {
     console.log("log> MongoDB connection FAILED!!!");
     console.log("log> Error:-");
     console.error(error);
-  });
+  }
+}
+
+// add middleware
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectDB();
+  }
+  next();
+});
+
+export default app;
