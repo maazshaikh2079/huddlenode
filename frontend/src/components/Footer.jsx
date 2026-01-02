e893d622f662394a2897f1f09ed7328cf49c41f3
maaz <maazshaikh2079@gmail.com>
2026-01-03 00:54:23 +0530

    23th commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,34 +1,29 @@
 import React from "react";
 import { logoTitle } from "../assets/assets.js";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="md:mx-10">
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      {/* Updated grid from 3 columns to 2 columns */}
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
           <img
             src={logoTitle}
-            alt="logoTitle"
-            className="w-50 mb-5 hover:opacity-80"
+            alt="HuddleNode Logo"
+            className="w-44 mb-5 hover:opacity-80 cursor-pointer"
+            onClick={() => navigate("/")}
           />
           <p className="w-full md:w-2/3 text-gray-600 leading-6">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            HuddleNode is a modern community forum empowering users to engage in
+            structured discussions. We prioritize privacy with our unique
+            identity shielding, providing a safe space for every voice.
           </p>
         </div>
 
-        <div>
-          <p className="text-xl font-medium mb-5">COMPANY</p>
-          <ul className="flex flex-col gap-2 text-gray-600">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Delivery</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </div>
-
+        {/* Keeping only GET IN TOUCH */}
         <div>
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-2 text-gray-600">
@@ -41,7 +36,7 @@ const Footer = () => {
       <div>
         <hr className="border-none" />
         <p className="py-5 text-sm text-center border-t border-t-zinc-400">
-          Copyright 2025 @ Prescripto.com - All Right Reserved.
+          Copyright 2026 @ HuddleNode.vercel.app - All Rights Reserved.
         </p>
       </div>
     </div>
